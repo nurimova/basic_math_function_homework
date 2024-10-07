d94032ca1ebbb39336b4b9f2796867b92412a874
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-10-07 09:46:38 +0500

    basic_func20.py

--- basic_func20.py
+++ basic_func20.py
@@ -1,11 +1,11 @@
 def main(a, b):
-    '''find the multiplication of a and b and return it.
+    '''logarifm a asosga ko'r b ning qiymatini toping.
     
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the result.
-    '''
+     Args:
+     a (int): raqam
+     b (int): raqam
+    
+     Qaytaradi:
+     int: natija.
+     '''
     return
